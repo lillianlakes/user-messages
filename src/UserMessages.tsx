28ff0eef585f390742f293c55c28aec48ec27833
Wukongthethird <simonzhang9518@gmail.com>
2021-09-07 16:05:45 -0700

    create user

--- src/UserMessages.tsx
+++ src/UserMessages.tsx
@@ -1,8 +1,25 @@
 import React, {useState} from "react";
-import { gql, useQuery, userMutation } from '@apollo/client';
+import { gql, useQuery, useMutation } from '@apollo/client';
+
+// const ADD_USER =gql`
+// mutation AddUser($username: Username!, $firstName: FirstName!, $lastName: LastName!){
+//     createUser(username: $username, first_name: $firstName, last_name: $lastName){
+//       username
+//       first_name
+//       last_name
+//     }
+//   }
+// `;
+
+interface Input  {
+  username:string,
+  firstName: string,
+  lastName: string,
+
+}
 
 const ADD_USER =gql`
-mutation AddUser($username: Username!, $firstName: FirstName!, $lastName: LastName!){
+mutation AddUser($username:ID!, $firstName: String!, $lastName: String!){
     createUser(username: $username, first_name: $firstName, last_name: $lastName){
       username
       first_name
@@ -15,22 +32,43 @@ function UserMessages() {
 
   // Forms
     // add user
-  let username, first_name, last_name;
+  
+  // let username ;
+  // let first_name;
+  // let last_name;
 
-  const [createUser, {data, loading, error}] = useMutation(ADD_USER, {
-    variable: {
-      username: "Username",
-      first_name: "First Name",
-      last_name: "Last Name"
-    },
-  });
 
-  if (loading) return 'Still loading...';
-  if (error) return `Submission error: ${error.message}`;
+
+  // const [createUser, {data, loading, error}] = useMutation(ADD_USER, {
+  //   variable: {
+  //     username: "Username",
+  //     first_name: "First Name",
+  //     last_name: "Last Name"
+  //   },
+  // });
+
+  const [ username, setUsername ] = useState<string>("")
+  const [ firstName, setFirstName ] = useState<string>("")
+  const [ lastName, setLastName ] = useState<string>("")
+
+
+
+
+  const [createUser, {data, loading, error} ] = useMutation(ADD_USER);
+
+
+  // if (loading) return 'Still loading...';
+  // if (error) return `Submission error: ${error.message}`;
 
   // For each user, form:
     // create message
     // delete message
+
+  function onSubmit(evt:React.FormEvent){
+    evt.preventDefault();
+    createUser( {variables:{username, firstName, lastName }} )
+  }
+
 
   return (
     <div>
@@ -38,30 +76,35 @@ function UserMessages() {
         onSubmit = { e => {
           e.preventDefault();
           createUser({ variables: { 
-            username: username.value, 
-            first_name: first_name.value, 
-            last_name: last_name.value
+            username: username ,
+            firstName: firstName, 
+            lastName: lastName
           }});
-          username.value = '';
-          first_name.value = '';
-          last_name.value = '';
         }}
         >
+   
         <input
-          ref = { node => {
-            username = node;
-          }}
+          id="username"
+          name="username"
+          onChange={ (evt) => setUsername(evt.target.value)}
+          value={username}
         />
+
         <input
-          ref = { node => {
-            first_name = node;
-          }}
+          id="firstName"
+          name="firstName"
+          onChange={ (evt) => setFirstName(evt.target.value)}
+          value={firstName}
         />
+
         <input
-          ref = { node => {
-            last_name = node;
-          }}
+          id="lastName"
+          name="lastName"
+          onChange={ (evt) => setLastName(evt.target.value)}
+          value={lastName}
         />
+
+
         <button type = "submit">Add User</button>
       </form>
     </div>    
